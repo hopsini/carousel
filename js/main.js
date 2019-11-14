bddd1ec52e350246429221596ae3ef2e3c125e5b
hopsini <56691002+hopsini@users.noreply.github.com>
2019-11-14 14:50:51 +0200

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+    $('.container').carousel({
+        num: 3,
+        maxWidth: 400,
+        maxHeight: 300,
+        distance: 150,
+        scale: 0.6,
+        animationTime: 1000,
+        showTime: 4000
+    });
+
+});
